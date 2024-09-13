fbe368ebee83924eaf7b8f2f9daff7774ed2023d
Pothumudi Sony Priya <61133478+Sonypriyasonu@users.noreply.github.com>
2024-09-13 14:04:56 +0530

    Create Zig Zag Shift.py

---
+++ Zig Zag Shift.py
@@ -0,0 +1,23 @@
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int(((n + 1)/2)-1)
+    print(mid)
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    ed = n - 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
+
+n = 6
+a = [1,2,3,4,5,6]
+findZigZagSequence(a, n)
