b418f7575b4b9fecce21b7b1ae3f6900a148385a
Pothumudi Sony Priya <61133478+Sonypriyasonu@users.noreply.github.com>
2024-09-13 13:42:19 +0530

    Update ab strings.py

--- ab strings.py
+++ ab strings.py
@@ -10,3 +10,10 @@ def abstr(a,b):
 a=input()
 b=input()
 print(abstr(a,b))
+
+'''24-Hour Time	
+Write a function that receives the time in 12-hour AM/PM format and 
+returns a string representation of the time in railway (24-hour) format.	
+convertTime(“07:05:45PM”) ➞ “19:05:45”
+convertTime(“12:40:22AM”) ➞ “00:40:22”
+convertTime(“12:45:54PM”) ➞ “12:45:54”'''
